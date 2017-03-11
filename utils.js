59cdc9ca5f01d2746164a02005485e4881693dca
Ignacio Losiggio <iglosiggio@gmail.com>
2017-03-11 14:36:26 -0300

    Style fixes on utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 module.exports = {
-  addToProto(ctor, props) {
-    for(const prop in props) {
-      ctor.prototype[prop] = props[prop];
-    }
-  }
+	addToProto(ctor, props) {
+		for(const prop in props) {
+			ctor.prototype[prop] = props[prop];
+		}
+	}
 }
