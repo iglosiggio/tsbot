1bc65248a7f574b5db5b2d792fce07a11b570b35
Ignacio Losiggio <iglosiggio@gmail.com>
2017-03-11 14:33:44 -0300

    Style fixes on plugins/core.js

--- plugins/core.js
+++ plugins/core.js
@@ -1,28 +1,28 @@
 module.exports = {
-  name: 'Core',
-  description: 'Core functionality for the TSBot',
-  commands: {
-    channel: {
-      list_plugins,
-      joinchannel
-    },
-    private: {
-      list_plugins,
-      followme,
-    }
-  },
+	name: 'Core',
+	description: 'Core functionality for the TSBot',
+	commands: {
+		channel: {
+			list_plugins,
+			joinchannel
+		},
+		private: {
+			list_plugins,
+			followme,
+		}
+	},
 
-  init(ts) {
-    console.log('Loading the core module!');
-  }
+	init(ts) {
+		console.log('Loading the core module!');
+	}
 }
 
 async function list_plugins(ts, message) {
-  const response = ts.plugins
-    .map(({name, description}) => name + ': ' + description)
-    .join('\n');
+	const response = ts.plugins
+		.map(({name, description}) => name + ': ' + description)
+		.join('\n');
 
-  return await message.respond(response);
+	return await message.respond(response);
 }
 
 async function followme(ts, message) {
@@ -36,6 +36,6 @@ async function joinchannel(ts, message) {
 	if(channel.cid) {
 		await ts.send('clientmove', { clid: 0, cid: channel.cid });
 	} else {
-    await message.respond('No se ha encontrado el channel "' + pattern + '"')
+		await message.respond('No se ha encontrado el channel "' + pattern + '"')
 	}
 }
